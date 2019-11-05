cdeb662bb71b7420912cc09653fe425faa0782f5
screp99 <peter.bemetz@t-online.de>
2019-11-05 13:16:44 +0100

    test diff

--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -1,6 +1,6 @@
 package model
 
-case class Field(column : Int, row: Int, value : Int) {
+case class Field1(column : Int, row: Int, value : Int) {
   def isBroken:Boolean = value == 0
   def isSet:Boolean = column <= 0 && row <= 0
 }
