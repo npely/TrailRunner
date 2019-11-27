55cfd615dbbb0bc487de985579b7c82596222149
PelyVPN <pelznik@gmail.com>
2019-11-27 17:18:03 +0100

    updated tests

--- src/test/scala/controller/ControllerSpec.scala
+++ src/test/scala/controller/ControllerSpec.scala
@@ -20,20 +20,31 @@ class ControllerSpec extends WordSpec with Matchers{
       }
       controller.add(observer)
       "notify its Observer after player makes a move" in {
+        controller.fieldIsBroken should be(true)
+        controller.fieldIsSet should be(true)
         player.yPos = 1
         controller.playerMoveUp()
+        controller.playerMoveRight()
+        controller.playerMoveDown()
+        controller.playerMoveLeft()
         observer.updated should be(true)
-        controller.player.yPos should be(0)
+        controller.player.yPos should be(1)
       }
       "notify its Observer when player stands on an other field" in {
         controller.playerStandsOnField()
         observer.updated should be(true)
-        controller.field.value should be(-1)
+        controller.field.value should be(0)
       }
       "notify its Observer when counter increases" in {
         controller.count
         observer.updated should be(true)
         controller.counter should be(1)
+      }
+      "should have a game string representation" in {
+        controller.playerToGameString should be ("P")
+      }
+      "should have a string representation" in {
+        controller.playerToString should be("Pete")
       }
     }
   }
