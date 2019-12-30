b8323847cb8a9bc9f81347c53371e72dd5f7b41f
PelyVPN <pelznik@gmail.com>
2019-12-30 13:37:28 +0100

    minor changes

--- src/main/scala/aview/gui/GameBuilder.scala
+++ src/main/scala/aview/gui/GameBuilder.scala
@@ -16,7 +16,6 @@ class GameBuilder(controller: Controller, gui: GUI) {
     contents += new Menu("File") {
       mnemonic = Key.F
       contents += new MenuItem(Action("Main menu") {
-        //gui.changeToMainMenu()
         controller.changeToMain()
       })
       contents += new MenuItem(Action("Quit") { System.exit(0) })
