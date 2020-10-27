84725ef4d396c2f2e3207a405edb31c8e8b764c7
Peter Bemetz <peter.bemetz@t-online.de>
2020-10-27 23:04:04 +0100

    :boom: added sound

--- src/main/scala/TrailRunner.scala
+++ src/main/scala/TrailRunner.scala
@@ -1,7 +1,10 @@
+import java.io.File
+
 import aview.TUI
 import aview.gui.GUI
 import com.google.inject.Guice
 import controller.controllerComponent.{ControllerInterface, DungeonChanged}
+import javax.sound.sampled.AudioSystem
 import src.main.TrailRunnerModule.TrailRunnerModule
 
 import scala.io.StdIn.readLine
@@ -16,10 +19,29 @@ object TrailRunner {
     def main(args: Array[String]): Unit = {
         var input: String = ""
 
+        playSound("src/main/scala/aview/gui/audio/8-bit_Dungeon.wav")
+
         do {
             input = readLine()
         } while (tui.evaluateInput(input) != -1)
 
         println("Bis zum nächsten Mal!")
+    }
+
+    def playSound(url: String): Unit = {
+        new Thread(new Runnable() { // The wrapper thread is unnecessary, unless it blocks on the
+            // Clip finishing; see comments.
+            override def run(): Unit = {
+                try {
+                    val clip = AudioSystem.getClip
+                    val inputStream = AudioSystem.getAudioInputStream(new File(url))
+                    clip.open(inputStream)
+                    clip.start()
+                } catch {
+                    case e: Exception =>
+                        System.err.println("failed to play audio: " + e.getMessage)
+                }
+            }
+        }).start()
     }
 }
