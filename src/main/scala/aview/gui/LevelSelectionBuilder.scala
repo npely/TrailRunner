0d26b24dc604adac4c5391d083d0c581fd2d4ad3
Peter Bemetz <peter.bemetz@t-online.de>
2021-01-14 11:20:13 +0100

    added level 7

--- src/main/scala/aview/gui/LevelSelectionBuilder.scala
+++ src/main/scala/aview/gui/LevelSelectionBuilder.scala
@@ -3,7 +3,7 @@ package aview.gui
 import controller.controllerComponent.ControllerInterface
 import controller.controllerComponent.controllerBaseImpl.Controller
 import model.levelComponent._
-import model.levelComponent.levelBaseImpl.{Level, Level1, Level2, Level3, Level4, Level5, Level6}
+import model.levelComponent.levelBaseImpl.{Level, Level1, Level2, Level3, Level4, Level5, Level6, Level7}
 
 import scala.swing.event.{ButtonClicked, Key}
 import scala.swing.{Action, BorderPanel, BoxPanel, Button, Dimension, Menu, MenuBar, MenuItem, Orientation}
@@ -58,6 +58,11 @@ class LevelSelectionBuilder(controller: ControllerInterface, gui: GUI) {
       new Button("Level 6") {
         reactions += {
           case e: ButtonClicked => controller.initializeGame(new Level6, false)
+        }
+      },
+      new Button("Level 7") {
+        reactions += {
+          case e: ButtonClicked => controller.initializeGame(new Level7, false)
         }
       }
     )
