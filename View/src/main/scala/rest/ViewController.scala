042b4cbb57ead3731ef801f6001acae0245d90ef
Peter Bemetz <peter.bemetz@t-online.de>
2021-04-20 22:38:28 +0200

    view api 5

--- View/src/main/scala/rest/ViewController.scala
+++ View/src/main/scala/rest/ViewController.scala
@@ -1,14 +1,6 @@
 package rest
 
-import akka.http.scaladsl.client.RequestBuilding.Get
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.server.RequestContext
-import akka.http.scaladsl.unmarshalling.Unmarshal
 import model.levelComponent.levelBaseImpl.Level
-import config.ModelJsonProtocol._
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-
-import scala.concurrent.Future
 
 object ViewController {
 
@@ -21,8 +13,6 @@ object ViewController {
   def save(): Boolean = ???
 
   def load(): Level = {
-    print(Get("http://localhost:8080/load").isResponse())
-    print(Get("http://localhost:8081/load").entity)
-    null
+    ???
   }
 }
