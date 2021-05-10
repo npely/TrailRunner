9683dbfec77c2c990fd7833ac87090ff31dcae59
Niklas Pelz <pelznik@gmail.com>
2021-05-10 16:04:07 +0200

    added ENV files and implemented save

--- Persistence/src/main/scala/persistence/MongoDB.scala
+++ Persistence/src/main/scala/persistence/MongoDB.scala
@@ -3,28 +3,45 @@ import model.levelComponent.levelBaseImpl.Level
 import org.mongodb.scala._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success, Try}
+import org.mongodb.scala.model.Filters._
 
 object MongoDB extends PersistenceInterface {
 
   val uri: String = "mongodb://mongo:27017/"
   val client: MongoClient = MongoClient(uri)
   val database: MongoDatabase = client.getDatabase("TrailRunner")
-  val collection: MongoCollection[Document] = database.getCollection("mongo")
+  val levelCollection: MongoCollection[Document] = database.getCollection("levels")
+  val fieldCollection: MongoCollection[Document] = database.getCollection("fields")
 
 
   override def load(): Level = {
-    val test = collection.find().first().head().onComplete({
+    /*val levelDocument = collection.find({ _id: "levelDocument"}).head().onComplete({
       case Success(value) => Some(value.toJson())
       case Failure(exception) => print(exception)
-    })
+    })*/
+    ???
   }
 
   override def save(level: Level): Boolean = {
-    val levelDocument: Document = Document("_id" -> level.name, "name" -> level.name, "playerX" -> level.player.xPos,
-      "playerY" -> level.player.yPos, "winX" -> level.winX, "winY" -> level.winY, "doorX" -> level.doorX, "doorY" -> level.doorY,
-      "isDoorOpen" -> level.isDoorOpen, "size" -> level.size)
+    levelCollection.deleteMany(equal("_id", "levelDocument"))
+    fieldCollection.deleteMany(equal("filter", "fieldDocument"))
+    val playerDocument: Document = Document("_id" -> "playerDocument", "playerX" -> level.player.xPos, "playerY" -> level.player.yPos)
+    for (x <- 0 to level.dungeon.length - 1) {
+      for (y <- 0 to level.dungeon.length - 1) {
+        if (level.player.xPos == x && level.player.yPos == y) {
+          fieldCollection.insertOne(Document("_id" -> x.toString.concat(y.toString), "name" -> "fieldDocument", "value" -> level.dungeon(x)(y).value,
+            "fieldType" -> level.dungeon(x)(y).fieldType, "fog" -> level.dungeon(x)(y).fog, "isPlayerOnField" -> true))
+        } else {
+          fieldCollection.insertOne(Document("_id" -> x.toString.concat(y.toString), "name" -> "fieldDocument", "value" -> level.dungeon(x)(y).value,
+            "fieldType" -> level.dungeon(x)(y).fieldType, "fog" -> level.dungeon(x)(y).fog, "isPlayerOnField" -> false))
+        }
+      }
+    }
+    val levelDocument: Document = Document("_id" -> "levelDocument", "name" -> level.name, "winX" -> level.winX,
+      "winY" -> level.winY, "doorX" -> level.doorX, "doorY" -> level.doorY,
+      "isDoorOpen" -> level.isDoorOpen, "size" -> level.size, "playerDocument" -> playerDocument)
     Try({
-      collection.insertOne(levelDocument)
+      levelCollection.insertOne(levelDocument)
     }) match {
       case Success(value) => true
       case Failure(exception) => println(exception); false
--- Persistence/src/main/scala/persistence/service/PersistenceController.scala
+++ Persistence/src/main/scala/persistence/service/PersistenceController.scala
@@ -2,16 +2,24 @@ package persistence.service
 
 
 import model.levelComponent.levelBaseImpl.Level
-import persistence.{FileIO, Slick}
+import persistence.{FileIO, Slick, MongoDB}
 
 object PersistenceController {
 
   def loadLastScore(): Level = {
-    Slick.load()
+    sys.env("DB_TYPE") match {
+      case "mongodb" => println("Loading from MongoDB"); MongoDB.load();
+      case "postgresql" => println("Loading from Slick"); Slick.load();
+      case _ => println("Loading from FileIO"); FileIO.load();
+    }
   }
 
   def saveLastScore(level: Level): Boolean = {
-    Slick.save(level)
+    sys.env("DB_TYPE") match {
+      case "mongodb" => println("Saving to MongoDB"); MongoDB.save(level);
+      case "postgresql" => println("Saving to Slick"); Slick.save(level);
+      case _ => println("Saving to FileIO"); FileIO.save(level);
+    }
   }
 
 }
